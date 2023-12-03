f25a174fd80c443718cbd6418e881b4286cb4aba
OleksandrRizun <137624723+OleksandrRizun@users.noreply.github.com>
2023-12-03 12:48:54 +0200

    ssssssd

--- src/pixabayapi.js
+++ src/pixabayapi.js
@@ -9,15 +9,16 @@ export class PixabayAPI {
         key: this.#API_KEY,
         image_type: "photo",
         orientation: "horizontal",
-        safesearch: "true",
+        safesearch: true,
         per_page: 40
     });
     async getImage () {
-        const url = `${this.#BASE_URL}?${this.#searchParams}&q=${this.#query}&page=${this.#page}`;
+        // const url = `${this.#BASE_URL}?${this.#searchParams}&q=${this.#query}&page=${this.#page}`;
+        const url = `https://pixabay.com/api?key=41032962-13a1f4b46a16f1c84256e5234&q="flower"&image_type="photo"&orientation="horizontal"&safesearch=true&per_page=40&page=1`;
         try {
             const data = await axios (url);
             return data;
-        } catch {
+        } catch (error) {
             console.error (error)
         }
     }
